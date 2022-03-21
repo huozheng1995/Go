a8bfd5cac0cce230be5e09c50ed40faaadb27302
huozheng1995 <339078795@qq.com>
2022-03-21 11:40:37 +0800

    utils

--- src/test/main.go
+++ src/test/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/edward/test/utils"
+)
 
 func main() {
-	fmt.Printf("OK: string=%v, bytes=%v\n", "ABCEFG", []byte("ABCEFG"))
+	utils.ReadFile("C:\\source\\binlog.000001")
+}
+
+func printASCII() {
+	fmt.Printf("OK: string=%v, bytes=%v\n", "ABCDEFG", []byte("ABCEFG"))
 	fmt.Printf("OK: string=%v, bytes=%v\n", "HIJKLMN", []byte("HIJKLMN"))
 	fmt.Printf("OK: string=%v, bytes=%v\n", "OPQRST", []byte("OPQRST"))
 	fmt.Printf("OK: string=%v, bytes=%v\n", "UVWXYZ", []byte("UVWXYZ"))
+	fmt.Printf("OK: string=%v, bytes=%v\n", "abcdefg", []byte("abcdefg"))
 }
