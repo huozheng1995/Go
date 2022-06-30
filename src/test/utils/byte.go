b53a891119fc0305d468326226bd6a4f10f3d4fb
huozheng1995 <339078795@qq.com>
2022-06-30 16:53:41 +0800

    Encoding

--- src/test/utils/byte.go
+++ src/test/utils/byte.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/binary"
 	"fmt"
+	"golang.org/x/text/encoding/charmap"
 	"math"
 	"strconv"
 	"strings"
@@ -12,63 +13,68 @@ var Endian = binary.BigEndian
 
 func FloatToBytes(val float32) []byte {
 	bits := math.Float32bits(val)
-	bytes := make([]byte, 4)
-	Endian.PutUint32(bytes, bits)
-	return bytes
+	arr := make([]byte, 4)
+	Endian.PutUint32(arr, bits)
+	return arr
 }
 
 func Float64ToBytes(val float64) []byte {
 	bits := math.Float64bits(val)
-	bytes := make([]byte, 8)
-	Endian.PutUint64(bytes, bits)
-	return bytes
+	arr := make([]byte, 8)
+	Endian.PutUint64(arr, bits)
+	return arr
 }
 
 func IntToBytes(val int64) []byte {
-	var bytes = make([]byte, 4)
-	Endian.PutUint32(bytes, uint32(val))
-	return bytes
+	var arr = make([]byte, 4)
+	Endian.PutUint32(arr, uint32(val))
+	return arr
 }
 
 func Int64ToBytes(val int64) []byte {
-	var bytes = make([]byte, 8)
-	Endian.PutUint64(bytes, uint64(val))
-	return bytes
+	var arr = make([]byte, 8)
+	Endian.PutUint64(arr, uint64(val))
+	return arr
 }
 
 func StringToBytes(val string) []byte {
 	return []byte(val)
 }
 
-func BytesToFloat(bytes []byte) float32 {
-	bits := Endian.Uint32(bytes)
+func BytesToFloat(arr []byte) float32 {
+	bits := Endian.Uint32(arr)
 	return math.Float32frombits(bits)
 }
 
-func BytesToFloat64(bytes []byte) float64 {
-	bits := Endian.Uint64(bytes)
+func BytesToFloat64(arr []byte) float64 {
+	bits := Endian.Uint64(arr)
 	return math.Float64frombits(bits)
 }
 
-func BytesToInt(bytes []byte) int {
-	return int(Endian.Uint32(bytes))
+func BytesToInt(arr []byte) int {
+	return int(Endian.Uint32(arr))
 }
 
-func BytesToInt64(bytes []byte) int64 {
-	return int64(Endian.Uint64(bytes))
+func BytesToInt64(arr []byte) int64 {
+	return int64(Endian.Uint64(arr))
 }
 
-func BytesToString(bytes []byte) string {
-	return string(bytes)
+func BytesToString(arr []byte) string {
+	return string(arr)
 }
 
-type BytesDataToNum func(bytes []byte) string
+func BytesToStringISO8859_1(arr []byte) string {
+	encoded, _ := charmap.ISO8859_1.NewDecoder().Bytes(arr)
+	return string(encoded)
+}
 
-func ByteArrayToInt8Array(bytes []byte) []int8 {
-	len := len(bytes)
+type BytesDataToNum func(arr []byte) string
+
+func ByteArrayToInt8Array(arr []byte) []int8 {
+	len := len(arr)
 	var result = make([]int8, len)
 	for i := 0; i < len; i++ {
-		result[i] = int8(bytes[i])
+		result[i] = int8(arr[i])
 	}
 	return result
 }
@@ -82,10 +88,10 @@ func Int8ArrayToByteArray(arr []int8) []byte {
 	return result
 }
 
-func ByteArrayToLine(bytes []byte) string {
+func ByteArrayToLine(arr []byte) string {
 	var result string
 	count := 0
-	for _, val := range bytes {
+	for _, val := range arr {
 		count++
 		if count%8 == 0 {
 			result = result + FillSpace(strconv.Itoa(int(val)), 3) + ", "
@@ -110,10 +116,10 @@ func Int8ArrayToLine(arr []int8) string {
 	return result
 }
 
-func ByteArrayToHex(bytes []byte) string {
+func ByteArrayToHex(arr []byte) string {
 	var result string
 	count := 0
-	for _, val := range bytes {
+	for _, val := range arr {
 		count++
 		if count%8 == 0 {
 			result = result + DecToHex(int64(val)) + ", "
