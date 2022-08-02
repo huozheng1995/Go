4c62aa389f9e4620a36d362b9e3ac835e1dbaefe
huozheng1995 <339078795@qq.com>
2022-08-02 17:03:47 +0800

    scp-294 improve logic

--- src/scp-294/utils/num.go
+++ src/scp-294/utils/num.go
@@ -7,7 +7,7 @@ import (
 var Endian = binary.BigEndian
 var ByteHexMap = map[byte]byte{0: 48, 1: 49, 2: 50, 3: 51, 4: 52, 5: 53, 6: 54, 7: 55, 8: 56, 9: 57, 10: 65, 11: 66, 12: 67, 13: 68, 14: 69, 15: 70}
 var ByteBinMap = map[byte]byte{0: 48, 1: 49}
-var CharNULL = "·"
+var CharNULL = "˴"
 
 func ByteToHex(val byte) string {
 	if val == 0 {
