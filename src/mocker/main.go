082ede47ffa4b834e9e081c249aa71b9c84894f7
Edward <edwardh@cdata.com>
2023-08-01 16:38:09 +0800

    Mocker: add network interface

--- src/mocker/main.go
+++ src/mocker/main.go
@@ -1,22 +1,28 @@
 package main
 
 type Settings struct {
-	MockerPort   int    `json:"MockerPort"`
-	ServerIP     string `json:"ServerIP"`
-	ServerPort   int    `json:"ServerPort"`
-	PrintDetails bool   `json:"PrintDetails"`
+	MockerPort                   int    `json:"MockerPort"`
+	ServerIP                     string `json:"ServerIP"`
+	ServerPort                   int    `json:"ServerPort"`
+	PrintDetails                 bool   `json:"PrintDetails"`
+	VirtualNetworkInterfaceMode  bool   `json:"VirtualNetworkInterfaceMode"`
+	LocalNetworkInterfaceAddress string `json:"LocalNetworkInterfaceAddress"`
 }
 
 func main() {
 	InitLog("mocker.log")
 
 	settings := Settings{
-		MockerPort:   11111,
-		ServerIP:     "adb-8439982502599436.16.azuredatabricks.net",
-		ServerPort:   443,
-		PrintDetails: true,
+		MockerPort:                   3306,
+		ServerIP:                     "172.16.85.140",
+		ServerPort:                   3306,
+		PrintDetails:                 true,
+		VirtualNetworkInterfaceMode:  true,
+		LocalNetworkInterfaceAddress: "172.16.85.1",
 	}
 	m := NewMocker(settings)
+	AddReqDataResData(m, []byte("a"), []byte("b"))
+	AddReqDataResData(m, []byte("c"), []byte("d"))
 	AddReqDataResData(m, []byte("abc\n"), []byte("def\n"))
 	//AddReqDataResData(m, HexFileToBytes("C:\\Users\\33907\\Downloads\\req.txt"), HexFileToBytes("C:\\Users\\33907\\Downloads\\res.txt"))
 	//AddResLenResData(m, 1945, HexFileToBytes("C:\\Users\\User\\Downloads\\SYNC-2347\\D_R_S\\log.log"))
